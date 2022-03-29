17e495f3b4d51a7f9fe02f9a9f7dbcb38ef08670
Christian Bunker <christian@bunkerplanet.com>
2022-03-29 10:24:05 -0400

    Update run_model32.py

--- run_model32.py
+++ run_model32.py
@@ -62,17 +62,16 @@ J = 0.1;
 #########################################################
 #### generation
 
-if True: # fig 6 ie T vs rho J a
+if False: # fig 6 ie T vs rho J a
     
     fig, ax = plt.subplots();
-    axins = inset_axes(ax, width="50%", height="50%");
-    Dvals = J*np.array([0,0.1,0.2,0.4])
+    Dvals = J*np.array([0,0.1,0.2,0.4]);
     for Di in range(len(Dvals)):
         D = Dvals[Di];
 
         # iter over rhoJ, getting T
         Tvals, Rvals = [], [];
-        rhoJavals = np.linspace(0.05,4.0,9);
+        rhoJavals = np.linspace(0.05,4.0,99);
         for rhoi in range(len(rhoJavals)):
 
             # energy
@@ -126,7 +125,7 @@ if True: # fig 6 ie T vs rho J a
         # plot
         Tvals, Rvals = np.array(Tvals), np.array(Rvals);
         #ax.plot(rhoJavals, Tvals[:,sourcei], label = "$|i\,>$", color = "black", linewidth = 2);
-        ax.plot(rhoJavals, Tvals[:,pair[0]], label = "$|+>$", color = colors[Di], linestyle = "dashed", linewidth = 2);
+        ax.plot(rhoJavals, Tvals[:,pair[0]], label = "$|+>$", color = colors[Di], linestyle = "solid", linewidth = 2);
         #ax.plot(rhoJavals, Tvals[:,pair[1]], label = "$|->$", color = "black", linestyle = "dashdot", linewidth = 2);
         ax.plot(rhoJavals, Tvals[:,0]+Tvals[:,1]+Tvals[:,2]+Rvals[:,0]+Rvals[:,1]+Rvals[:,2], color = "red");
 
@@ -139,6 +138,7 @@ if True: # fig 6 ie T vs rho J a
     rhoJalims = np.array([rhoJavals[0], rhoJavals[-1]]);
     Elims = J*J/(rhoJalims*rhoJalims*np.pi*np.pi*tl) - 2*tl;
     Evals = np.linspace(Elims[-1], Elims[0], len(rhoJavals)); # switched !
+    #print(">>>", rhoJalims, "\n>>>", Evals); assert False;
     for Di in range(len(Dvals)):
         D = Dvals[Di];
 
@@ -189,34 +189,38 @@ if True: # fig 6 ie T vs rho J a
             
         # plot
         Tvals, Rvals = np.array(Tvals), np.array(Rvals);
-        axins.plot(Evals,Tvals[:,pair[0]], color = colors[Di], linestyle = "dashed", linewidth = 2); # + state
+        axins.plot(Evals,Tvals[:,pair[0]], color = colors[Di], linestyle = "solid", linewidth = 2); # + state
+    
+    axins.set_xlim(-2,-1.6);
+    axins.set_xticks([-2,-1.8,-1.6]);
+    axins.set_xlabel("$E/t$", fontsize = "x-large");
+    axins.set_ylim(0,0.15);
+    axins.set_yticks([0,0.15]);
+    axins.set_ylabel("$T_+$", fontsize = "x-large");
 
     # format and show
-    axins.set_xlim(min(Evals)-0.01,max(Evals));
-    axins.set_xticks([-2,-1.6]);
-    axins.set_xlabel("$E/t$", fontsize = "x-large");
-    axins.set_ylim(0,0.25);
-    axins.set_yticks([0,0.25]);
-    axins.set_ylabel("$T$");
-    ax.set_xlim(min(rhoJavals),max(rhoJavals));
-    ax.set_xticks([0,1,2,3,4]);
+    ax.set_xlim(0,4);
+    ax.set_xticks([0,2,4]);
     ax.set_xlabel("$J/\pi \sqrt{tE_b}$", fontsize = "x-large");
-    ax.set_ylim(0,0.25);
-    ax.set_yticks([0,0.25]);
-    ax.set_ylabel("$T$", fontsize = "x-large");
+    ax.set_ylim(0,0.15);
+    ax.set_yticks([0,0.15]);
+    ax.set_ylabel("$T_+$", fontsize = "x-large");
     plt.show();
 
 if False: # T vs E
 
     # main plot T vs E
     fig, ax = plt.subplots();
-    Dvals = J*np.array([0,0.1,0.2,0.4])
+    Dvals = J*np.array([0,0.1]); # ,0.2,0.4])
     for Di in range(len(Dvals)):
         D = Dvals[Di];
 
         # iter over Energy, getting T
         Tvals, Rvals = [], [];
-        Evals = np.linspace(-2+0.0001,-2+0.1,99)
+        rhoJalims = np.array([0.05,4.0]);
+        #Evals = np.linspace(-2+0.0001,-2+0.1,9);
+        Elims = J*J/(rhoJalims*rhoJalims*np.pi*np.pi*tl) - 2*tl;
+        Evals = np.linspace(Elims[-1], Elims[0], 9); 
         for Ei in range(len(Evals)):
 
             # energy
@@ -268,7 +272,7 @@ if False: # T vs E
         ax.plot(Evals, Tvals[:,0]+Tvals[:,1]+Tvals[:,2]+Rvals[:,0]+Rvals[:,1]+Rvals[:,2], color = "red")
 
     # now do T vs rhoJa inset plot
-    if False:
+    if True:
         axins = inset_axes(ax, width="50%", height="50%");
     else:
         Dvals = [];
@@ -277,9 +281,8 @@ if False: # T vs E
 
         # iter over rhoJ, getting T
         Tvals, Rvals = [], [];
-        Elims = np.array([Evals[0], Evals[-1]]);
-        rholims = J/(np.pi*np.sqrt((Elims+ 2*tl)*tl));
-        rhoJavals = np.linspace(rholims[-1], rholims[0], len(Evals)); # switched !
+        #print(Elims,"\n",rhoJalims); assert False
+        rhoJavals = np.linspace(rhoJalims[-1], rhoJalims[0], len(Evals)); # switched !
         for rhoi in range(len(rhoJavals)):
 
             # energy
@@ -327,21 +330,20 @@ if False: # T vs E
         Tvals, Rvals = np.array(Tvals), np.array(Rvals);
         axins.plot(rhoJavals, Tvals[:,pair[0]], label = "$|+>$", color = colors[Di], linestyle = "dashed", linewidth = 2);
 
-    axins.set_xlim(min(rhoJavals), max(rhoJavals));
-    axins.set_xticks([0,1]);
+    axins.set_xlim(0,4);
+    axins.set_xticks([0,2,4]);
     axins.set_xlabel("$J/\pi \sqrt{tE_b}$", fontsize = "x-large");
     axins.set_ylim(0,0.25);
     axins.set_yticks([0,0.25]);
     axins.set_ylabel("$T$", fontsize = "x-large");
 
     # format and show
-    ax.set_xlim(min(Evals),max(Evals));
-    ax.set_xticks([-2,-1.9]);
+    ax.set_xlim(-2,-1.6);
+    ax.set_xticks([-2,-1.8,-1.6]);
     ax.set_xlabel("$E/t$", fontsize = "x-large");
     ax.set_ylim(0,0.25);
     ax.set_yticks([0,0.25]);
     ax.set_ylabel("$T$", fontsize = "x-large");
-    #plt.legend();
     plt.show();
 
 
