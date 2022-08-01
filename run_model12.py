50137d49e45b307714bbecc052bad02839faa17d
Christian Bunker <christian@bunkerplanet.com>
2022-07-31 22:31:45 -0400

    displays T minus with inset

--- run_model12.py
+++ run_model12.py
@@ -23,7 +23,7 @@ import sys
 # top level
 np.set_printoptions(precision = 4, suppress = True);
 verbose = 5;
-pair = (1,2);
+pair = (1,2); # pair[0] is the + state
 sourcei = 4;
 
 # fig standardizing
@@ -32,7 +32,7 @@ mycolors = ["black","darkblue","darkgreen","darkred", "darkmagenta","darkgray","
 mystyles = ["solid", "dashed","dotted","dashdot"];
 mylinewidth = 1.0;
 mypanels = ["(a)","(b)","(c)"];
-plt.rcParams.update({"text.usetex": True,"font.family": "Times"})
+#plt.rcParams.update({"text.usetex": True,"font.family": "Times"})
 
 ##################################################################################
 #### entanglement generation (cicc Fig 6)
@@ -88,7 +88,7 @@ if False: # compare T vs rhoJa for N not fixed
     np.save(fname, data);
 
 
-if True: # compare T vs rhoJa for N=2 fixed
+if False: # compare T vs rhoJa for N fixed
 
     # siam inputs
     tl = 1.0;
@@ -97,61 +97,87 @@ if True: # compare T vs rhoJa for N=2 fixed
     # choose boundary condition
     source = np.zeros(8); # incident up, imps = down, down
     source[4] = 1;
-    spinstate = "baa"
+    spinstate = "baa";
 
-    # iter over E, getting T
-    Tvals, Rvals = [], [];
-    #rhoJavals = np.linspace(xlims[0], xlims[1], 99);
-    logElims = -4,-1
-    Evals = np.logspace(*logElims,199);
-    for Eval in Evals:
+    for DeltaVg in tl*np.array([1,1.5,2,2.5,3]):
 
-        # energy and K fixed by J, rhoJ
-        #E_rho = Jeff*Jeff/(rhoJa*rhoJa*np.pi*np.pi*tl); # fixed E that preserves rho_J_int
-                                                # this E is measured from bottom of band !!!
-        Energy = Eval - 2*tl;
-        
-        # optical distances, N = 2 fixed
-        ka = np.arccos((Energy)/(-2*tl));
-        Vg = Energy + 2*tl; # gate voltage
-        N0 = 1;
+        # iter over E, getting T
+        logElims = -5,-1
+        Evals = np.logspace(*logElims,199);
+        Rvals = np.empty((len(Evals),len(source)), dtype = float);
+        Tvals = np.empty((len(Evals),len(source)), dtype = float);
+        for Evali in range(len(Evals)):
 
-        # construct hams
-        # since t=tl everywhere, can use h_cicc_eff to get LL, RL blocks directly
-        i1, i2 = 1, 1+N0;
-        hblocks, tnn = wfm.utils.h_cicc_eff(Jeff, tl, i1, i2, i2+2, pair);
-        hblocks[1] += Vg*np.eye(len(source)); # Vg shift in SR
-        hblocks[2] += Vg*np.eye(len(source));
-        tnnn = np.zeros_like(tnn)[:-1]; # no next nearest neighbor hopping
-        if(verbose > 3 and Eval == Evals[0]): print(hblocks);
+            # energy and K fixed by J, rhoJ
+            #E_rho = Jeff*Jeff/(rhoJa*rhoJa*np.pi*np.pi*tl); # fixed E that preserves rho_J_int
+                                                    # this E is measured from bottom of band !!!
 
-        # get T from this setup
-        Tvals.append(wfm.kernel(hblocks, tnn, tnnn, tl, Energy , source));
-        Rvals.append(wfm.kernel(hblocks, tnn, tnnn, tl, Energy , source, reflect = True));
+            # energy
+            Eval = Evals[Evali];
+            Energy = Eval - 2*tl;
+            
+            # optical distances, N = 2 fixed
+            ka = np.arccos((Energy)/(-2*tl));
+            #Vg = Energy + 2*tl; # gate voltage
+            Vg = Energy + DeltaVg;
+            N0 = 2;
 
-    # save data to .npy
-    Tvals, Rvals = np.array(Tvals), np.array(Rvals);
-    data = np.zeros((2+2*len(source),len(Evals)));
-    data[0,0] = tl;
-    data[0,1] = Jeff;
-    data[1,:] = Evals;
-    data[2:10,:] = Tvals.T;
-    data[10:,:] = Rvals.T;
-    fname = "data/model12/N2";
-    print("Saving data to "+fname);
-    np.save(fname, data);
+            # construct hams
+            # since t=tl everywhere, can use h_cicc_eff to get LL, RL blocks directly
+            i1, i2 = 1, 1+N0;
+            hblocks, tnn = wfm.utils.h_cicc_eff(Jeff, tl, i1, i2, i2+2, pair);
+            hblocks[1] += Vg*np.eye(len(source)); # Vg shift in SR
+            hblocks[2] += Vg*np.eye(len(source));
+            tnnn = np.zeros_like(tnn)[:-1]; # no next nearest neighbor hopping
+            if(verbose > 3 and Eval == Evals[0]): print(hblocks);
+
+            # get T from this setup
+            Rdum, Tdum = wfm.kernel(hblocks, tnn, tnnn, tl, Energy , source);
+            Rvals[Evali] = Rdum;
+            Tvals[Evali] = Tdum;
+
+        # save data to .npy
+        data = np.zeros((2+2*len(source),len(Evals)));
+        data[0,0] = tl;
+        data[0,1] = Jeff;
+        data[1,:] = Evals;
+        data[2:10,:] = Tvals.T; # 8 spin dofs
+        data[10:,:] = Rvals.T;
+        fname = "data/model12/N"+str(N0)+"_"+str(DeltaVg);
+        print("Saving data to "+fname);
+        np.save(fname, data);
 
 
 ########################################################################
-#### plot data
 
-# open command line file
-datafs = sys.argv[1:];
-fig, axes = plt.subplots(1,len(datafs), sharey = True);
-fig.set_size_inches(7/2,6/2);
-if( len(datafs)== 1): axes = [axes];
-for fi in range(len(datafs)):
-    dataf = datafs[fi];
+#### plot T_- / T_+ at different Vg
+if False:
+    fig, ax = plt.subplots();
+    datafs = sys.argv[1:];
+    for datafi in range(len(datafs)):
+        dataf = datafs[datafi];
+        print("Loading data from "+dataf);
+        data = np.load(dataf);
+        tl = data[0,0];
+        Jeff = data[0,1];
+        xvals = data[1];
+        Tvals = data[2:10];
+        Rvals = data[10:];
+        #ax.plot(xvals, Tvals[pair[1]]/Tvals[pair[0]],color = mycolors[datafi],linewidth = mylinewidth);
+        ax.plot(xvals, Tvals[pair[1]],color = mycolors[datafi],linewidth = mylinewidth);
+        ax.set_xscale('log', subs = []);
+        ax.set_xlim(10**(-5), 10**(-1));
+        ax.set_xticks([10**(-5),10**(-4),10**(-3),10**(-2),10**(-1)]);
+
+    # format
+    ax.set_xlabel('$(E+2t)/t$',fontsize = myfontsize);
+    ax.set_ylabel('$T$', fontsize = myfontsize);  
+    plt.tight_layout();
+    plt.show();
+
+#### plot cicc-like data
+if True:
+    dataf = sys.argv[1];
     print("Loading data from "+dataf);
     data = np.load(dataf);
     tl = data[0,0];
@@ -162,26 +188,78 @@ for fi in range(len(datafs)):
     totals = np.sum(Tvals, axis = 0) + np.sum(Rvals, axis = 0);
     print("- shape xvals = ", np.shape(xvals));
     print("- shape Tvals = ", np.shape(Tvals));
-    print(np.max(Tvals[pair[0]]))
 
-    # plot
-    axes[fi].set_title(mypanels[fi], x=0.12, y = 0.88);
-    axes[fi].plot(xvals, Tvals[sourcei],color = "black", linestyle = "solid",linewidth = mylinewidth);
-    axes[fi].plot(xvals, Tvals[pair[0]], color = "black", linestyle = "dashed", linewidth = mylinewidth);
-    #axes[fi].plot(xvals, Tvals[pair[1]], color = "black", linestyle = "dotted", linewidth = mylinewidth);
-    axes[fi].plot(xvals, totals, color="red");
-    axes[fi].set_xscale('log', subs = []);
-    axes[fi].set_xlim(10**(-4), 10**(-1));
-    axes[fi].set_xticks([10**(-4),10**(-3),10**(-2),10**(-1)])
-    axes[fi].set_xlabel('$(E+2t)/t$',fontsize = myfontsize);
-    
-# format
-axes[0].set_ylim(0,1.0);
-axes[0].set_yticks([0,0.5,1]);
-axes[0].set_ylabel('$T$', fontsize = myfontsize);  
-plt.tight_layout();
-plt.show();
-#plt.savefig('model12.pdf');
+    # plot 3 possibilities
+    sigmas = [sourcei,pair[0], pair[1]];
+    mainax = plt.subplot(3,1,(1,2));
+    for sigmai in range(len(sigmas)):
+        mainax.plot(xvals, Tvals[sigmas[sigmai]],color = mycolors[sigmai],linewidth = mylinewidth);
+        mainax.set_xscale('log', subs = []);
+
+    # inset zoom in
+    insax = plt.subplot(3,1,3);
+    insax.plot(xvals, Tvals[pair[1]],color = mycolors[2],linewidth = mylinewidth);
+    insax.set_xscale('log', subs = []);
+    insax.set_xlim(10**(-5), 10**(-1));
+    insax.set_xticks([10**(-5),10**(-4),10**(-3),10**(-2),10**(-1)])
+    insax.set_xlabel('$(E+2t)/t$',fontsize = myfontsize);
+
+    # format
+    mainax.sharex(insax);
+    mainax.label_outer();
+    insax.label_outer();
+    mainax.set_ylim(0,1.0);
+    mainax.set_yticks([0,0.5,1]);
+    mainax.set_ylabel('$T$', fontsize = myfontsize);
+    insax.set_ylim(0,2*10**(-3));
+    insax.set_yticks([0,10**(-3),2*10**(-3)]);
+    insax.set_ylabel('$T_-$', fontsize = myfontsize);  
+    plt.tight_layout();
+    plt.show();
+    #plt.savefig('model12.pdf');
+
+
+
+
+#### plot data side by side
+if False:
+    # open command line file
+    datafs = sys.argv[1:];
+    fig, axes = plt.subplots(1,len(datafs), sharey = True);
+    fig.set_size_inches(7/2,6/2);
+    if( len(datafs)== 1): axes = [axes];
+    for fi in range(len(datafs)):
+        dataf = datafs[fi];
+        print("Loading data from "+dataf);
+        data = np.load(dataf);
+        tl = data[0,0];
+        Jeff = data[0,1];
+        xvals = data[1];
+        Tvals = data[2:10];
+        Rvals = data[10:];
+        totals = np.sum(Tvals, axis = 0) + np.sum(Rvals, axis = 0);
+        print("- shape xvals = ", np.shape(xvals));
+        print("- shape Tvals = ", np.shape(Tvals));
+        print(np.max(Tvals[pair[0]]))
+
+        # plot
+        axes[fi].set_title(mypanels[fi], x=0.12, y = 0.88);
+        axes[fi].plot(xvals, Tvals[sourcei],color = "black", linestyle = "solid",linewidth = mylinewidth);
+        axes[fi].plot(xvals, Tvals[pair[0]], color = "black", linestyle = "dashed", linewidth = mylinewidth);
+        #axes[fi].plot(xvals, Tvals[pair[1]], color = "black", linestyle = "dotted", linewidth = mylinewidth);
+        axes[fi].plot(xvals, totals, color="red");
+        axes[fi].set_xscale('log', subs = []);
+        axes[fi].set_xlim(10**(-4), 10**(-1));
+        axes[fi].set_xticks([10**(-4),10**(-3),10**(-2),10**(-1)])
+        axes[fi].set_xlabel('$(E+2t)/t$',fontsize = myfontsize);
+        
+    # format
+    axes[0].set_ylim(0,1.0);
+    axes[0].set_yticks([0,0.5,1]);
+    axes[0].set_ylabel('$T$', fontsize = myfontsize);  
+    plt.tight_layout();
+    plt.show();
+    #plt.savefig('model12.pdf');
 
 
 
